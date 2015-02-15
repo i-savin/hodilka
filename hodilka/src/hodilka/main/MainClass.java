a3787231a57a1bf7bf9d96e962a370f2acb90595
ayrapetovai <ayeaye.post@gmal.com>
2015-02-15 16:58:04 +0300

    comment in main class fixed

--- hodilka/src/hodilka/main/MainClass.java
+++ hodilka/src/hodilka/main/MainClass.java
@@ -1,6 +1,6 @@
 package hodilka.main;
 
-/** Entry point class */
+/** Entry point class, run as Java application */
 public class MainClass {
 
 	public static void main(String[] args) throws Exception {
