3cfda9f49344dcc8d253feae822257e97eba7560
ayrapetovai <ayeaye.post@gmal.com>
2015-02-23 02:00:10 +0300

    performance issue...

--- hodilka/src/hodilka/output/OutputSystem.java
+++ hodilka/src/hodilka/output/OutputSystem.java
@@ -6,17 +6,24 @@ public class OutputSystem {
 	
 	private OutputInterface consoleInterface;
 	private Model model;
+	private long lastRendered;
 	
 	public OutputSystem(OutputInterface consoleInterface, Model model) {
 		this.consoleInterface = consoleInterface;
 		this.model = model;
+		lastRendered = System.currentTimeMillis();
 	}
 	
 	public void render() {
-		consoleInterface.clear();
-		
-		consoleInterface.draw();
-		
-		consoleInterface.flush();
+		if (System.currentTimeMillis() - lastRendered > 30) {
+			
+			consoleInterface.clear();
+			
+			consoleInterface.draw();
+			
+			consoleInterface.flush();
+			
+			lastRendered = System.currentTimeMillis();
+		}
 	}
 }
